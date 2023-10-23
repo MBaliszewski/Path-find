ef59cf946e9d380adbfe5e05dea887afd4a1e11b
Mikołaj <mikolaj.baliszewski@wp.pl>
2023-10-23 11:41:15 +0200

    Corrected heuristics

--- A_star.py
+++ A_star.py
@@ -25,7 +25,7 @@ class Node:
         self.h = None
 
     def heuristics(self, x_end, y_end):
-        return (x_end - self.x) + (y_end - self.y)
+        return abs(x_end - self.x) + abs(y_end - self.y)
     
     def __str__(self):
         return f'Node {self.id}: x={self.x}, y={self.y}'
@@ -34,7 +34,7 @@ class Edge:
     def __init__(self, fromn: Node, to: Node):
         self.fromn = fromn
         self.to = to
-        self.cost = abs(int((self.fromn.weight + self.to.weight) / 2))
+        self.cost = int((self.fromn.weight + self.to.weight) / 2)
 
 def make_graph(file_path):
     file = open(file_path, 'r')
