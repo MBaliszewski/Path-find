12ea9ab7fd63062f9cb365792f008c3481a4487e
Mikołaj <mikolaj.baliszewski@wp.pl>
2023-11-03 11:34:26 +0100

    Done read_roads.py

--- dataStructures.py
+++ dataStructures.py
@@ -6,11 +6,8 @@ class Graph:
     def __str__(self):
         string = ''
         for node in self.nodes:
-            w = node.weight
-            e = [edge.to.weight for edge in node.edges]
             c = [edge.cost for edge in node.edges]
-            h = node.h
-            string += f'Node {node.id}: x={node.x}, y={node.y}, w={w}, Edges to: {e}, Costs: {c}, Heuristic: {h}\n'
+            string += f'Node {node.id}: x={node.x}, y={node.y}, costs: {c}\n'
             
         return string
             
@@ -29,7 +26,7 @@ class Node:
         return f'Node {self.id}: x={self.x}, y={self.y}'
 
 class Edge:
-    def __init__(self, fromn: Node, to: Node, length: float):
+    def __init__(self, fromn: Node, to: Node, cost: float):
         self.fromn = fromn
         self.to = to
-        self.cost = length
+        self.cost = cost
--- read_roads.py
+++ read_roads.py
@@ -28,20 +28,33 @@ def make_graph(workspace, layer):
                 node_start = Node(id=generator_node_id, x=x_start, y=y_start)
                 nodes[generator_node_id] = node_start
                 index_nodes[generate_id_from_xy(node_start.x, node_start.y)] = generator_node_id
+                generator_node_id += 1
 
-            # to samo dla geom.lastPoint
+            x_end =  geom.lastPoint.X
+            y_end =  geom.lastPoint.Y
+            if generate_id_from_xy(x_end, y_end) in index_nodes:
+                node_end = nodes[index_nodes[generate_id_from_xy(x_end, y_end)]]
+            else:
+                node_end = Node(id=generator_node_id, x=x_end, y=y_end)
+                nodes[generator_node_id] = node_end
+                index_nodes[generate_id_from_xy(node_end.x, node_end.y)] = generator_node_id
+                generator_node_id += 1
 
-            # edge = Edge(from=node_start, to=node_from, cost=length)
-            # node_start.edges.append(edge)
-            # node_end.edges.append(edge)
+            edge = Edge(fromn=node_start, to=node_end, cost=length)
+            edges[idx] = edge
+            node_start.edges.append(edge)
+            node_end.edges.append(edge)
+            # print(node_start)
+            # print(idx)
+            # print(f'Start: {geom.firstPoint.X}, {geom.firstPoint.Y}')
+            # print(f'End: {geom.lastPoint.X}, {geom.lastPoint.Y}')
+            # print(f'Len: {row[2]}')
 
-            print(idx)
-            print(f'Start: {geom.firstPoint.X}, {geom.firstPoint.Y}')
-            print(f'End: {geom.lastPoint.X}, {geom.lastPoint.Y}')
-            print(f'Len: {row[2]}')
+    return Graph(edges=list(edges.values()), nodes=list(nodes.values()))
 
 
 workspace = 'dane\\torun'
-#layer = 'L4_1_BDOT10k__OT_SKJZ_L.shp'
-layer = 'testowa.shp'
-make_graph(workspace, layer)
+layer = 'L4_1_BDOT10k__OT_SKJZ_L.shp'
+#layer = 'testowa.shp'
+graph = make_graph(workspace, layer)
+print(graph)
